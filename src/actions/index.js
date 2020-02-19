81f1b4b951d2b837c933de1f21ec3b897963e7a3
paulwstorm <anapaulorocket@gmail.com>
2020-02-19 18:14:28 -0500

    Added initial files and ability to test api in testing folder

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const FETCH_FOOD = "fetch_food";
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
+}
---
+++ src/testing/testing-main.js
@@ -0,0 +1,6 @@
+const apiEndpoint = `https://trackapi.nutritionix.com/v2/natural/nutrients`;
+const params = { query: "chicken" };
+const headers = { "x-app-key": '3dc2ad864ff3cc39020f1dd6b5b5fbeb', "x-app-id": '1cba2023'}
+const  nutrientInfo = axios.post(apiEndpoint, params, {
+  headers,
+});
