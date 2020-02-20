87d9010a355455bac0049554eaad97e61b67e43f
BENZABOSS <joyceben33@gmail.com>
2020-02-20 12:42:47 -0500

    Setup the column headers.

--- src/components/viewMeals.js
+++ src/components/viewMeals.js
@@ -7,8 +7,30 @@ import React, { Component } from "react";
 
 class ViewMeals extends Component {
     render() {
-        return(
-            <div>Welcome to the View Meals Page</div>
+        return (
+            <html lang="en">
+                <header>
+                    <h1 className="text-center">Username's Meal</h1>
+                </header>
+                <body>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-3" >
+                                <h5>Breakfast</h5>
+                            </div>
+                            <div className="col-3">
+                                <h5>Lunch</h5>
+                            </div>
+                            <div className="col-3">
+                                <h5>Dinner</h5>
+                            </div>
+                            <div className="col-3">
+                                <h5>Snacks</h5>
+                            </div>
+                        </div>
+                    </div>
+                </body>
+            </html>
         )
     }
 
