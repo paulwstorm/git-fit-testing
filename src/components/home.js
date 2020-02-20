30fccd23a4e83dc37f2d74a8f984fb79177e0422
BENZABOSS <joyceben33@gmail.com>
2020-02-20 11:46:55 -0500

    Added to the Home Page Component.

--- src/components/home.js
+++ src/components/home.js
@@ -2,13 +2,21 @@ import _ from "lodash";
 import React, { Component } from "react";
 // import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import { fetchPosts } from "../actions";
 
 class Home extends Component {
     render() {
         return(
-            <div>Welcome to the Home Page</div>
+            <div>
+                <h1>Git-Fit App</h1>
+                <br />
+                <h4>Enter Info Below</h4>
+                <input placeholder="Username"></input>
+                <input placeholder="Calorie Goal"></input>
+                <Link path="/meals" ><button className="btn btn-primary submit-button">Submit</button></Link>
+                
+            </div>
         )
     }
 
