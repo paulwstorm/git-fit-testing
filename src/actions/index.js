8f15177feba17b87b1f99ea2000d571de63778dc
paulwstorm <anapaulorocket@gmail.com>
2020-02-20 15:07:40 -0500

    Added addFoodItem aciton.

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,27 +14,34 @@ export function newUser(user) {
   };
 }
 
-// export function addFoodItem(food, meal) {
-//   switch (meal) {
-//     case breakfast:
-//       return {
-//         type: ADD_BREAKFAST_ITEM,
-//         payload: food
-//       }
-//     case lunch:
-//       return {
-//         type: ADD_LUNCH_ITEM,
-//         payload: food
-//       }
-//     case dinner:
-//       return {
-//         type: ADD_DINNER_ITEM,
-//         payload: food
-//       }
-//     case snack:
-//       return {
-//         type: ADD_SNACK_ITEM,
-//         payload: food
-//       }
-//   }
-// }
+export function addFoodItem(query, meal) {
+  const apiEndpoint = `https://trackapi.nutritionix.com/v2/natural/nutrients`;
+  const params = { query: query };
+  const headers = { "x-app-key": '3dc2ad864ff3cc39020f1dd6b5b5fbeb', "x-app-id": '1cba2023'}
+  const food = axios.post(apiEndpoint, params, {
+    headers,
+  });
+
+  switch (meal) {
+    case "breakfast":
+      return {
+        type: ADD_BREAKFAST_ITEM,
+        payload: food
+      }
+    case "lunch":
+      return {
+        type: ADD_LUNCH_ITEM,
+        payload: food
+      }
+    case "dinner":
+      return {
+        type: ADD_DINNER_ITEM,
+        payload: food
+      }
+    case "snack":
+      return {
+        type: ADD_SNACK_ITEM,
+        payload: food
+      }
+  }
+}
