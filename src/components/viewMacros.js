9f2078c5313991db85c6f40af88fd27b85360dbf
Jay Averitt <jayaveritt@gmail.com>
2020-02-21 01:31:48 -0500

    Adding HighCharts Mock-up

--- src/components/viewMacros.js
+++ src/components/viewMacros.js
@@ -12,6 +12,49 @@ class ViewMacros extends Component {
         <h3> Your Carbohydrates Total: </h3>
         <h3> Your Protein Total: </h3>
         <h3> Your Fat Total: </h3>
+
+        {/* ghcharts.chart('container', {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie'
+  },
+  title: {
+    text: 'Your Daily Macros'
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+      }
+    }
+  },
+  series: [{
+    name: 'Macro Categories',
+    colorByPoint: true,
+    data: [{
+      name: 'Carbs',
+      y: 31.41,
+      sliced: true,
+      selected: true
+    }, {
+      name: 'Protein',
+      y: 21.84
+    }, {
+      name: 'Fat',
+      y: 16.85
+    }, 
+  }]
+}); */}
+
+
       </div>
 
         )
